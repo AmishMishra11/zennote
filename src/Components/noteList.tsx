07f7d5d99b494eb631c1c7f95fbb2898dc496a81
Amish Mishra <amishmishra11@gmail.com>
2025-05-28 21:43:17 +0530

    Initial commit

---
+++ src/Components/noteList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { Note } from "../App";
+
+type noteListProps = {
+  notes: Note[];
+};
+
+function NoteList({ notes }: noteListProps) {
+  console.log(notes);
+  return (
+    <main>
+      <h2 className="text-2xl">Notes</h2>
+
+      {notes.length === 0 ? (
+        <p className="text-gray-500">No notes available</p>
+      ) : (
+        notes.map((notes) => <p>{notes.title}</p>)
+      )}
+    </main>
+  );
+}
+
+export default NoteList;
